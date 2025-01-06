05aef0a7a50c5313be99c4571a03070be1d35427
shivdatta dixit <shivdattadixit0567@gmail.com>
2025-01-06 19:19:58 +0530

    deploy

--- frontend/src/services/users/userServices.js
+++ frontend/src/services/users/userServices.js
@@ -7,8 +7,16 @@ const token = getUserFromStorage();
 console.log(token);
 
 export const loginApi = async (userData) => {
-  const response = await axios.post(`${apiUrl}/users/login`, userData);
-  return response.data;
+  const response = await fetch(`${apiUrl}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+  // const response = await axios.post(`${apiUrl}/users/login`, userData);
+  const data = await response.json();
+  return data;
 };
 
 export const registerApi = async (userData) => {
