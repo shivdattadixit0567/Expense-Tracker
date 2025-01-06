6c9e4ac1878bdd1dcbbc22185ed51521a1be6fe3
shivdatta dixit <shivdattadixit0567@gmail.com>
2025-01-06 19:51:11 +0530

    deploy

--- frontend/src/services/users/userServices.js
+++ frontend/src/services/users/userServices.js
@@ -1,22 +1,14 @@
 import axios from "axios";
 import { getUserFromStorage } from "../../utils/getUserFromStorage";
-const apiUrl = import.meta.env.VITE_BASE_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
 
 // console.log(BASE_URL);
 const token = getUserFromStorage();
 console.log(token);
 
 export const loginApi = async (userData) => {
-  const response = await fetch(`${apiUrl}/users/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-  // const response = await axios.post(`${apiUrl}/users/login`, userData);
-  const data = await response.json();
-  return data;
+  const response = await axios.post(`${apiUrl}/users/login`, userData);
+  return response.data;
 };
 
 export const registerApi = async (userData) => {
